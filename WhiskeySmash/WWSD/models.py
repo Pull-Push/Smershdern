ab18aeabfb029e8489e709166422edace1791018
Jeff Sokol <JeffSokolDev@gmail.com>
2024-05-15 14:36:23 -0400

    scorched earth policy

--- WhiskeySmash/WWSD/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class SmashData(models.Model):
-    fighterNumber = models.CharField(max_length=255)
-    fighterName = models.CharField(max_length=255)
