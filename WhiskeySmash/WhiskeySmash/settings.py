6b59898c294b7ccd4e640a6cbd3463fc157e7d8f
Jeff Sokol <JeffSokolDev@gmail.com>
2024-06-13 20:31:32 -0400

    minor db work

--- WhiskeySmash/WhiskeySmash/settings.py
+++ WhiskeySmash/WhiskeySmash/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'WhiskeySmash.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'sampledjango',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "smash",
+        "USER": "jeffreysokol",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
