f3ec66cb1896879033f2705f3ccc73d0a983fc6e
Jeff Sokol <JeffSokolDev@gmail.com>
2024-06-19 11:57:33 -0400

    more DB stuff

--- WhiskeySmash/WhiskeySmash/settings.py
+++ WhiskeySmash/WhiskeySmash/settings.py
@@ -79,13 +79,9 @@ WSGI_APPLICATION = 'WhiskeySmash.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "smash",
-        "USER": "jeffreysokol",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'smashsqlite',
     }
 }
 
--- WhiskeySmash/whiskeywheel/models.py
+++ WhiskeySmash/whiskeywheel/models.py
@@ -1,24 +1,28 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
-class TestClass(models.Model):
-    name = models.CharField(max_length=255)
+# class TestClass(models.Model):
+#     name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.first
+#     def __str__(self):
+#         return self.first
 
 
 
-class PersonClass(models.Model):
-    first = models.CharField(max_length=255),
-    last = models.CharField(max_length=255)
+# class PersonClass(models.Model):
+#     first = models.CharField(max_length=255),
+#     last = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.first
+#     def __str__(self):
+#         return self.first
     
-class ww_member(models.Model):
-    member_id = models.IntegerField(),
-    member_name = models.CharField(max_length=255)
+# class ww_member(models.Model):
+#     id = models.IntegerField(),
+#     member_id = models.IntegerField()
+#     member_name = models.CharField(max_length=255)
+#     date_created = models.DateField(default=datetime.now, blank=False)
+#     date_modified = models.DateField(auto_now=True, blank=True)
 
-    def __str__(self):
-        return self.member_name
+#     def __str__(self):
+#         return self.member_name
